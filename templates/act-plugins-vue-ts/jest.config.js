52ed79a7b09057322e93877b85bbe20d1b0bb386
魏晓东 <2758570254@qq.com>
2025-09-22 11:53:48 +0800

    feat: 优化 agent

--- templates/act-plugins-vue-ts/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'ts', 'vue'],
-  transform: {
-    '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.[tj]s$': 'ts-jest'
-  }
-};
